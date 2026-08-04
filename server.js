9a118fbf601e564e77945434ac3f7782c78e1c7f
Irene Casas Ortega <ireneco@cs.washington.edu>
2026-08-04 14:29:26 -0700

    added mongodb driver, backend connection, npm, etc - server works and needs CRUD plus more changes which goes toward front end

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { connectDB } = require("./db/connection");
+
+// const resourceRoutes = require("./routes/resources");
+// const savedResourceRoutes = require("./routes/savedResources");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/resources", resourceRoutes);
+// app.use("/api/saved-resources", savedResourceRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Something went wrong" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err.message);
+  });
